d531b8646362cd2043413506f8b0a9d9cb199c27
jackson@near.org <jacksonnear.org@jacksonnearorgs-MacBook-Pro.local>
2024-07-02 20:37:53 -0700

    fix: typo

--- apps/ticketing/src/stores/stripe.ts
+++ apps/ticketing/src/stores/stripe.ts
@@ -20,7 +20,6 @@ type StripeStore = {
 function uuidv4() {
   if (crypto === undefined) return;
   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-    // eslint-disable-next-line
     (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
   );
 }
