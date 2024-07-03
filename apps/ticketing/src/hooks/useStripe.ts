635e419a04dcd48495966e3878d65bc75c4b0fc0
jackson@near.org <jacksonnear.org@jacksonnearorgs-MacBook-Pro.local>
2024-07-02 20:41:48 -0700

    fix: merge conflicst

--- apps/ticketing/src/hooks/useStripe.ts
+++ apps/ticketing/src/hooks/useStripe.ts
@@ -6,6 +6,7 @@ import { useStripeStore } from '@/stores/stripe';
 import { EVENTS_WORKER_BASE, KEYPOM_MARKETPLACE_CONTRACT_ID } from '@/utils/common';
 
 function uuidv4() {
+  if (crypto === undefined) return;
   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
     (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
   );
