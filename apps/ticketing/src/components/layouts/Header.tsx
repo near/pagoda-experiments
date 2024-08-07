def9bad0566a13d41083c04b90f443963fb2d9dc
jackson@near.org <jackson.harris3@gmail.com>
2024-08-07 01:54:39 -0700

    feat: hide sign-in on pruchase page and disable ticket logo route

--- apps/ticketing/src/components/layouts/Header.tsx
+++ apps/ticketing/src/components/layouts/Header.tsx
@@ -6,33 +6,39 @@ import { useWalletStore } from '@/stores/wallet';
 
 import { AccountDropdown } from '../AccountDropdown';
 import s from './Header.module.scss';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
   const account = useWalletStore((store) => store.account);
+  const router = useRouter();
+  const eventRoute = router.pathname.includes('/events');
+  const isDisabled = eventRoute ? s.disabled : '';
+  console.log('eventRoute', eventRoute);
   return (
     <header className={s.header}>
-      <Container className={s.container}>
-        <Link href="/" className={s.logo}>
-          <SvgIcon icon={<QrCode weight="duotone" />} size="m" />
-          Ticketing
-        </Link>
+      <div className={isDisabled}>
+        <Container className={s.container}>
+          <Link href={'/'} className={s.logo}>
+            <SvgIcon icon={<QrCode weight="duotone" />} size="m" />
+            Ticketing
+          </Link>
 
-        {account && (
-          <div className={s.links}>
-            <Link href="/events/new" className={s.link}>
-              <SvgIcon icon={<PlusCircle weight="regular" />} />
-              <span>Create Event</span>
-            </Link>
+          {account && (
+            <div className={s.links}>
+              <Link href="/events/new" className={s.link}>
+                <SvgIcon icon={<PlusCircle weight="regular" />} />
+                <span>Create Event</span>
+              </Link>
 
-            <Link href="/events" className={s.link}>
-              <SvgIcon icon={<CalendarDots weight="regular" />} />
-              <span>Manage Events</span>
-            </Link>
-          </div>
-        )}
-
-        <AccountDropdown />
-      </Container>
+              <Link href="/events" className={s.link}>
+                <SvgIcon icon={<CalendarDots weight="regular" />} />
+                <span>Manage Events</span>
+              </Link>
+            </div>
+          )}
+          {!account && eventRoute ? null : <AccountDropdown />}
+        </Container>
+      </div>
     </header>
   );
 };
